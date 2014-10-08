5b6a726bbc65ad9fabf2f171fbf7c26e7b87904b
Anthony Caccia <acaccia@ulb.ac.be>
2014-10-08 23:02:06 +0200

    that was such a one-liner, this 29th problem, OMG !

--- Euler/Euler29/main.hs
+++ Euler/Euler29/main.hs
@@ -4,9 +4,5 @@ import           Control.Applicative
 import           Data.List           (group, sort)
 import           System.IO
 
-filterDistincts = map head . group . sort
-
-allThoseShit = (^) <$> [2..100] <*> [2..100]
-
-main = print $ length $ filterDistincts allThoseShit
+main = print $ (length . group . sort) $ (^) <$> [2..100] <*> [2..100]
 
