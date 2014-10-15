4c76495990410b242a8b027dc5fcbe596f74ea04
Anthony Caccia <acaccia@ulb.ac.be>
2014-10-15 21:18:21 +0200

    Problem 49 is a little bit more readable

--- Euler/Euler49/main.hs
+++ Euler/Euler49/main.hs
@@ -4,8 +4,13 @@ import           Data.List           (sort)
 import           Data.Numbers.Primes (isPrime)
 import           System.IO           (print)
 
-main = print $ head $ map (\(a,b,c) -> show a ++ show b ++ show c) andTheAnswerIs
+main = print $ tail $ map (\(a,b,c) -> show a ++ show b ++ show c) andTheAnswerIs
 
-andTheAnswerIs = [(a, a + 3330, a + 6660)  | a <- [1000..7000], and $ map isPrime [a, a + 3330, a + 6660], a + 6660 < 10000, thisIsDumb a == thisIsDumb (a + 3330), thisIsDumb a == thisIsDumb (a + 6660), a /= 1487]
-                where thisIsDumb = sort . show
+andTheAnswerIs = [(a, a + 3330, a + 6660)  | a <- [1000..7000]
+                    , and $ map isPrime [a, a + 3330, a + 6660]
+                    , a + 6660 < 10000
+                    , thisIsDumb a == thisIsDumb (a + 3330)
+                    , thisIsDumb a == thisIsDumb (a + 6660)
+                 ]
+                    where thisIsDumb = sort . show
 
